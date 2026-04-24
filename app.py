926f67ee661b6207b2894aca442829e3eb9cca2b
eslambth <eslamalhajji@stu.topkapi.edu.tr>
2026-04-24 20:28:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+for m in genai.list_models():
+    if 'generateContent' in m.supported_generation_methods:
+        print(m.name)
+
+
+
+
+
+
+
 import streamlit as st
 import google.generativeai as genai
 import urllib.parse
